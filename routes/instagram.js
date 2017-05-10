d9fadc3d1d20cddefb7b9213b90832a49505dc29
Brandon Da Silva <da.silva.brandon@gmail.com>
2017-05-10 13:02:40 -0400

    Testing Webflow

--- routes/instagram.js
+++ routes/instagram.js
@@ -4,6 +4,12 @@
 var express = require('express');
 var router = express.Router();
 
+const Webflow = require('webflow-api')
+const webflow = new Webflow({ token: process.env.WEBFLOW_TOKEN })
+
+webflow.info()
+  .then(info => console.log(info))
+
 router.get ('/', function(req, res) {
   res.set('Access-Control-Allow-Origin', '*');
   res.send('API v1 GET: Hello World!');

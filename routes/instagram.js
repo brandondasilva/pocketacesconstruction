f8457fa4dab97dfa1e3ef4b3c2a54414cfedead4
Brandon Da Silva <da.silva.brandon@gmail.com>
2017-05-10 17:10:33 -0400

    Testing Webflow posting

--- routes/instagram.js
+++ routes/instagram.js
@@ -7,9 +7,6 @@ var router = express.Router();
 const Webflow = require('webflow-api')
 const webflow = new Webflow({ token: process.env.WEBFLOW_TOKEN })
 
-webflow.info()
-  .then(info => console.log(info))
-
 router.get ('/', function(req, res) {
   res.set('Access-Control-Allow-Origin', '*');
   res.send('API v1 GET: Hello World!');
@@ -18,7 +15,29 @@ router.get ('/', function(req, res) {
 router.post ('/', function(req, res) {
   res.set('Access-Control-Allow-Origin', '*');
 
+  var item = webflow.createItem({
+    collectionId: '5904f80595a2d43d313758fc',
+    fields: {
+      'name': req.body['name'],
+      'slug': req.body['slug'],
+      '_archived': false,
+      '_draft': false,
+      'post-link': req.body['link'],
+      'image-link': req.body['image']
+    },
+    live: true
+  })
+
   res.send(req.body);
 });
 
 module.exports = router;
+
+
+// 58e6d0fa9d08c00e607f27fb
+
+// curl https://api.webflow.com/collections/5904f80595a2d43d313758fc/items \
+//     -H "Authorization: Bearer c3571a7ebfbcbefbfe02346b708be8ef78c5c4fb9782b6eb6a6bbf4481cf929f" \
+//     -H 'accept-version: 1.0.0'
+//
+// c3571a7ebfbcbefbfe02346b708be8ef78c5c4fb9782b6eb6a6bbf4481cf929f

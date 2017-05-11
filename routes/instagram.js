e0f8e89f579171dd7b102fd0b8d02c2a90a1a03c
Brandon Da Silva <da.silva.brandon@gmail.com>
2017-05-10 21:02:49 -0400

    Edits

--- routes/instagram.js
+++ routes/instagram.js
@@ -75,6 +75,7 @@ router.post ('/', function(req, res) {
         }
       ]
     }, function (error, response, body) {
+      console.log(body);
       if (!error && response.statusCode == 200) {
         console.log(body);
       }
@@ -91,6 +92,15 @@ module.exports = router;
 
 // 58e6d0fa9d08c00e607f27fb
 
+// curl 'https://pocketacesconstruction.herokuapp.com/instagram' \
+//   -H "Content-Type: application/json" \
+//   -X POST -d '
+//     "name": "Exciting blog post title",
+//     "slug": "exciting-post-3",
+//     "post-link": "https://www.instagram.com/p/BT4NSOZjHyD/",
+//     "image-link": "https://scontent.cdninstagram.com/t51.2885-15/e35/p320x320/18382487_1958594714427116_8724567892445626368_n.jpg"
+//   '
+
 // curl https://api.webflow.com/collections/5904f80595a2d43d313758fc/items \
 //     -H "Authorization: Bearer c3571a7ebfbcbefbfe02346b708be8ef78c5c4fb9782b6eb6a6bbf4481cf929f" \
 //     -H 'accept-version: 1.0.0'
@@ -100,14 +110,14 @@ module.exports = router;
 // curl 'https://api.webflow.com/collections/5904f80595a2d43d313758fc/items' \
 //   -H "Authorization: Bearer c3571a7ebfbcbefbfe02346b708be8ef78c5c4fb9782b6eb6a6bbf4481cf929f" \
 //   -H 'accept-version: 1.0.0' \
-//   -H "Content-Type: application/json" \
-//   --data-binary $'{
-//       "fields": {
-//         "name": "Exciting blog post title",
-//         "slug": "exciting-post-3",
-//         "_archived": false,
-//         "_draft": false,
-//         "post-link": "https://www.instagram.com/p/BT4NSOZjHyD/",
-//         "image-link": "https://scontent.cdninstagram.com/t51.2885-15/e35/p320x320/18382487_1958594714427116_8724567892445626368_n.jpg"
-//       }
-//     }'
+  // -H "Content-Type: application/json" \
+  // --data-binary $'{
+  //     "fields": {
+  //       "name": "Exciting blog post title",
+  //       "slug": "exciting-post-3",
+  //       "_archived": false,
+  //       "_draft": false,
+  //       "post-link": "https://www.instagram.com/p/BT4NSOZjHyD/",
+  //       "image-link": "https://scontent.cdninstagram.com/t51.2885-15/e35/p320x320/18382487_1958594714427116_8724567892445626368_n.jpg"
+  //     }
+  //   }'

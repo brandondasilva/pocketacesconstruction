b76848d281ee647e9b5daff2bc442f879539f04e
Brandon Da Silva <da.silva.brandon@gmail.com>
2017-05-10 21:46:44 -0400

    Finished Instagram integration

--- routes/instagram.js
+++ routes/instagram.js
@@ -16,11 +16,6 @@ router.get ('/', function(req, res) {
 router.post ('/', function(req, res) {
   res.set('Access-Control-Allow-Origin', '*');
 
-  console.log('first check start');
-  console.log(req.body.name);
-  console.log(req.body['name']);
-  console.log('first check end');
-
   var item = webflow.createItem({
     collectionId: '5904f80595a2d43d313758fc',
     fields: {
@@ -33,22 +28,7 @@ router.post ('/', function(req, res) {
     }
   });
 
-  // var slackResponse = "A new Instagram post has been posted and uploaded to Webflow:\n\n\n";
-  // slackResponse += "Name: " + req.body['name'] + "\n\n";
-  // slackResponse += "Post Link: " + req.body['link'] + "\n\n";
-  // slackResponse += "Image Link: " + req.body['image'] + "\n\n";
-  // slackResponse += "This needs to be published to the Webflow CMS";
-
-  // request.post(
-  //   'https://hooks.slack.com/services/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',
-  //   { json: { "text": slackResponse } },
-  //   function (error, response, body) {
-  //     if (!error && response.statusCode == 200) {
-  //       console.log(body);
-  //     }
-  //   }
-  // )
-
+  // HTTP POST to Slack Webhook to post an update on Slack
   request({
     url: "https://hooks.slack.com/services/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",
     method: "POST",
@@ -60,7 +40,7 @@ router.post ('/', function(req, res) {
           "color": "#36a64f",
           "pretext": "A new post from Instagram has been posted to Webflow.",
           "title": "Instagram Post to Webflow",
-          "text": "This needs to be published to the Webflow CMS via the Webflow Editor",
+          "text": "This needs to be published to the Webflow CMS using the Webflow Editor",
           "fields": [
             {
               "title": "Name",
@@ -79,7 +59,6 @@ router.post ('/', function(req, res) {
         }
       ]
     }, function (error, response, body) {
-      console.log(body);
       if (!error && response.statusCode == 200) {
         console.log(body);
       }
@@ -92,36 +71,3 @@ router.post ('/', function(req, res) {
 });
 
 module.exports = router;
-
-
-// 58e6d0fa9d08c00e607f27fb
-
-// curl 'https://pocketacesconstruction.herokuapp.com/instagram' \
-//   -H "Content-Type: application/json" \
-//   -X POST -d '{
-//     "name": "Exciting blog post title",
-//     "slug": "exciting-post-3",
-//     "post-link": "https://www.instagram.com/p/BT4NSOZjHyD/",
-//     "image-link": "https://scontent.cdninstagram.com/t51.2885-15/e35/p320x320/18382487_1958594714427116_8724567892445626368_n.jpg"
-//   }'
-
-// curl https://api.webflow.com/collections/5904f80595a2d43d313758fc/items \
-//     -H "Authorization: Bearer c3571a7ebfbcbefbfe02346b708be8ef78c5c4fb9782b6eb6a6bbf4481cf929f" \
-//     -H 'accept-version: 1.0.0'
-//
-// c3571a7ebfbcbefbfe02346b708be8ef78c5c4fb9782b6eb6a6bbf4481cf929f
-
-// curl 'https://api.webflow.com/collections/5904f80595a2d43d313758fc/items' \
-//   -H "Authorization: Bearer c3571a7ebfbcbefbfe02346b708be8ef78c5c4fb9782b6eb6a6bbf4481cf929f" \
-//   -H 'accept-version: 1.0.0' \
-  // -H "Content-Type: application/json" \
-  // --data-binary $'{
-  //     "fields": {
-  //       "name": "Exciting blog post title",
-  //       "slug": "exciting-post-3",
-  //       "_archived": false,
-  //       "_draft": false,
-  //       "post-link": "https://www.instagram.com/p/BT4NSOZjHyD/",
-  //       "image-link": "https://scontent.cdninstagram.com/t51.2885-15/e35/p320x320/18382487_1958594714427116_8724567892445626368_n.jpg"
-  //     }
-  //   }'

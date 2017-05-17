2298768b449f01b6402189c8bbba1706c55b157f
Brandon Da Silva <brandon@bdsdesign.co>
2017-05-17 14:18:13 -0400

    Edits

--- routes/quote.js
+++ routes/quote.js
@@ -211,11 +211,11 @@ function authorize(callback) {
   console.log(url.parse(AuthUrl));
 
   oauth2Client.setCredentials({
-    access_token: process.env.GOOGLE_ACCESS_TOKEN,
     refresh_token: process.env.GOOGLE_REFRESH_TOKEN
   });
 
   oauth2Client.refreshAccessToken(function(err, tokens) {
+    console.log('before tokens');
     console.log(tokens);
   })
 

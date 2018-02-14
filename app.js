6255fa5563e078e54a1706fb276c246cff8015a9
Brandon Da Silva <brandon@bdsdesign.co>
2018-02-13 20:39:58 -0500

    Added proper authentication

--- app.js
+++ app.js
@@ -4,11 +4,21 @@
 const express = require('express');
 const PORT = process.env.PORT || 3000;
 
+var auth = require('http-auth');
+var basic = auth.basic({
+  realm: "Restricted Access!"
+}, function(username, password, callback) {
+  callback((username === process.env.AUTH_USER && password === process.env.AUTH_PASS));
+});
+
 var app = express();
 var bodyParser = require('body-parser');
 var helmet = require('helmet');
 
 app.set('port', PORT);
+
+app.use(auth.connect(basic));
+var server = require('http').createServer(basic);
 
 app.use(express.static(__dirname + '/public'));
 

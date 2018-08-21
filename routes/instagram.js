e9483e2acaadb50c0211f6bd0a59b7db92b73fb9
Brandon Da Silva <brandon@bdsdesign.co>
2018-08-21 09:19:26 -0400

    Testing image uploads for Instagram posts

--- routes/instagram.js
+++ routes/instagram.js
@@ -35,6 +35,7 @@ router.post ('/', function(req, res) {
       '_archived': false,
       '_draft': false,
       'post-link': req.body['link'],
+      'main-image': req.body['image'],
       'image-link': req.body['image']
     }
   });

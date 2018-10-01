10b337f7b0f706890b25a8c09644b747ac21d222
Brandon Da Silva <brandon@bdsdesign.co>
2018-10-01 18:40:07 -0400

    Added template files and email functionality Ready for testing

--- routes/facebook.js
+++ routes/facebook.js
@@ -3,6 +3,9 @@
 
 var moment = require('moment-timezone');
 
+var ejs = require('ejs');
+var fs = require('fs');
+
 var express = require('express');
 var request = require('request');
 var router = express.Router();
@@ -19,7 +22,82 @@ router.get ('/', function(req, res) {
 router.post ('/', function(req, res) {
   res.set('Access-Control-Allow-Origin', '*');
 
+  var templateFile = "";
+  var template = "";
+
+  if (req.body['company'] == "pac") {
+
+    templateFile = ejs.compile(fs.readFileSync(__dirname + '/../views/facebook/pac.ejs', 'utf8'));
+    template = templateFile({
+      name: req.body['name'],
+      email: req.body['email'],
+      date: req.body['date'],
+      phone: req.body['phone'],
+      city: req.body['city'],
+      jobtype: req.body['jobtype']
+    });
+
+  } else if (req.body['company'] == "pare") {
+
+    templateFile = ejs.compile(fs.readFileSync(__dirname + '/../views/facebook/pare.ejs', 'utf8'));
+    template = templateFile({
+      name: req.body['name'],
+      email: req.body['email'],
+      date: req.body['date'],
+      phone: req.body['phone'],
+      servicetype: req.body['servicetype']
+    });
+
+  }
+
+  sendgridRequest(composeMail(req.body['email'], template));
 
 });
+
+/**
+ * Set up the mail information and template to be requested to be sent through SendGrid
+ *
+ * @param {String} from_email "From" email
+ * @param {String} message Template file for email message
+ */
+function composeMail(from_email, message) {
+
+  return sg.emptyRequest({
+    method: 'POST',
+    path: '/v3/mail/send',
+    body: {
+      personalizations: [
+        {
+          to: [{
+            email: 'da.silva.brandon@gmail.com'
+          }],
+          subject: "New Facebook Form Submission"
+        }
+      ],
+      from: { email: from_email },
+      content: [{
+        type: 'text/html',
+        value: message
+      }]
+    }
+  });
+}
+
+/**
+ * Sends the SendGrid request to the API
+ *
+ * @param {Object} req The callback to call
+ */
+function sendgridRequest(req) {
+
+  sg.API(req, function(error, response) {
+    // Log response
+    console.log('--RESPONSE BEGIN--');
+    console.log(response.statusCode);
+    console.log(response.body);
+    console.log(response.headers);
+    console.log('--RESPONSE END--\n');
+  });
+}
 
 module.exports = router;
